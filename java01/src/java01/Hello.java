802f5711de33f7efae201eb1053f2c93366e03e6
bitacademy <bitacademy@bitacademy-PC>
2014-06-03 14:51:06 +0900

    .

--- java01/src/java01/Hello.java
+++ java01/src/java01/Hello.java
@@ -2,8 +2,10 @@ package java01;
 
 public class Hello {
 
-	public static void main(String[] args) {
-		System.out.println("Hello!");
+	public static void main(String[] args) {                                            
+		System.out.println("안녕하세요");
+		System.out.println("okok"); 
+		System.out.println("nono");
 	}
 
 }
