f2e970421f2668e84cdbb0fb598a9bfc203031c1
gh <gh@gh-PC>
2014-06-12 14:31:34 +0900

    .

---
+++ java01/src/exam/oop2/step03/Truck.java
@@ -0,0 +1,6 @@
+package exam.oop2.step03;
+
+public class Truck extends Car {
+	int weight;//중량
+
+}
--- java01/src/exam/oop2/step04/Car.java
+++ java01/src/exam/oop2/step04/Car.java
@@ -2,7 +2,7 @@
  * 상속에서 인스턴스 변수?
  *  - sub클래스의 인스턴스를 만들 때 super클래스의 인스턴스 변수까지 생성된다.
  */
-package exam.oop2.step03;
+package exam.oop2.step04;
 
 public class Car {
 	String model;//모델명
