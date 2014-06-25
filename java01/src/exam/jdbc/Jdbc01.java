54203ef010b962dce1af3e7cdc597046726d0976
gh <gh@gh-PC>
2014-06-25 11:45:49 +0900

    .

---
+++ java01/src/exam/jdbc/Jdbc01.java
@@ -0,0 +1,21 @@
+package exam.jdbc;
+/*
+ * 역할자
+ *  - 어떤 기능들을 수행하는 클래스를 지칭
+ *  
+ */
+public class Jdbc01 {
+
+	public static void main(String[] args) {
+		//1. Driver 역할자를 로딩
+		
+		//2. DriverManager를 통해 connection 역할자를 얻기
+		//3. Connection 역할자를 통해 Statement 역할자를 얻기
+		//4. Statement  역할자를 통해 SQL 서버에 보냄
+		//5. ResultSet 역할자를 통해 서버에서 레코드를 하나씩 가져옴
+		//6. ResultSet 역할자 닫기
+		//7. Statement 역할자 닫기
+		//8. Connection 역할자 끊기
+	}
+
+}
