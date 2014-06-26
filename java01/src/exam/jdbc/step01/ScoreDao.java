df2e35ac332b3d0d0a3cc997e01f41ed0428aa17
gh <gh@gh-PC>
2014-06-26 09:13:34 +0900

    .

--- java01/src/exam/jdbc/step01/ScoreDao.java
+++ java01/src/exam/jdbc/step01/ScoreDao.java
@@ -1,22 +1,42 @@
-/* 배열 대신 ArrayList 적용 
- */
 package exam.jdbc.step01;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
+import java.sql.ResultSet;
 
 public class ScoreDao {
+	
+	static int cursor;
+	Connection con;
+	Statement stmt;
+	ResultSet rs;
 
   public ScoreDao() {
+	  try{
+		  Class.forName("com.mysql.jdbc.Driver");
+		  
+		    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bitdb", "bit","1111" );
+		    stmt = con.createStatement();
+		    rs = stmt.executeQuery("select* from scores where sno = (select max(SNO) from scores);");
+		    
+		    if(rs.next()){
+		    	 cursor = rs.getInt("sno")+1;
+		    }
+		    rs.close();
+		    stmt.close();
+		    con.close();
+	  } catch(Exception e){
+		  e.printStackTrace();
+	  }
   }
 
   public void insert(Score score) {
 	  try{
 		  Class.forName("com.mysql.jdbc.Driver");
-		   
-		    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bitdb", "bit","1111" );
-		    Statement stmt = con.createStatement();
+		  
+		    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bitdb", "bit","1111" );
+		    stmt = con.createStatement();
 		    
 		    int count = stmt.executeUpdate(
 		    		"insert into scores (name, kor, eng, math) "
@@ -28,6 +48,7 @@ public class ScoreDao {
 		    if(count == 1){
 		    	System.out.println("입력완료!");
 		    }
+
 		    stmt.close();
 		    con.close();
 	  } catch(Exception e){
@@ -37,22 +58,62 @@ public class ScoreDao {
   }
 
   public Score nextScore() {
-
-    return null;
+	 Score score = new Score();
+	 
+	  try{
+		  Class.forName("com.mysql.jdbc.Driver");
+		   
+		   con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bitdb", "bit","1111" );
+		   stmt = con.createStatement();
+		   rs = stmt.executeQuery("select* from scores where sno >" + cursor++ +" order by sno asc Limit 1");
+		    
+		   if(!rs.next()){
+			   cursor--;
+			   return null;
+		    }
+		    score.setName(rs.getString("name"));
+		    score.setKor(rs.getInt("kor"));
+		    score.setEng(rs.getInt("eng"));
+		    score.setMath(rs.getInt("math"));
+		   
+		    rs.close();
+		    stmt.close();
+		    con.close();
+		    
+	  } catch(Exception e){
+		  e.printStackTrace(); //어떤예외가 발생했는지 상세하게 출력
+	  }
+	  return score;
   }
 
   public Score previousScore() {
-      return null;
+	  
+	  Score score = new Score();
+	  
+	  try{ 
+		  Class.forName("com.mysql.jdbc.Driver");
+		   
+		    con = DriverManager.getConnection("jdbc:mysql://localhost:3306/bitdb", "bit","1111" );
+		    stmt = con.createStatement();
+		    rs = stmt.executeQuery("select* from scores where sno <" + cursor-- +" order by sno desc Limit 1");
+		 
+		   if(!rs.next()){
+			   cursor = 1;
+			   return null;	
+		    }
+		   score.setName(rs.getString("name"));
+		   score.setKor(rs.getInt("kor"));
+		   score.setEng(rs.getInt("eng"));
+		   score.setMath(rs.getInt("math"));
+		   
+		    rs.close();
+		    stmt.close();
+		    con.close();
+		    
+	} catch(Exception e){
+		  e.printStackTrace(); //어떤예외가 발생했는지 상세하게 출력
+	}
+	return score;
   }
-
+	 
 }
-
-
-
-
-
-
-
-
-
-
